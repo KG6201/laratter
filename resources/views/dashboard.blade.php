3d8cd60bd3111d74be25dcdc9435243886cd84d4
kneg <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2022-08-31 02:07:55 +0900

    add fired!

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,9 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    And you're fired!
+                </div>
             </div>
         </div>
     </div>
